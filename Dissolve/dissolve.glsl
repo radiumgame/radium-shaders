f257a3e191d4b15e35dfff7904f66a5a5b83b30d
LandonHarter <landonharter@outlook.com>
2022-04-28 11:58:56 -0400

    Dissolve shader

---
+++ Dissolve/dissolve.glsl
@@ -0,0 +1,17 @@
+#version 330 core
+
+out vec4 fragColor;
+
+uniform sampler2D MainTex;
+uniform float noiseScale;
+uniform float heightCutoff;
+
+void main() {
+    vec2 sample = gl_FragCoord.xy;
+    sample /= noiseScale;
+    float noise = perlin(sample);
+    float cutoff = worldPosition.y + noise;
+    float alpha = step(heightCutoff, cutoff);
+    fragColor = texture(MainTex, texture_coordinate);
+    fragColor.a = alpha;
+}
--- Hologram/hologram.glsl
+++ Hologram/hologram.glsl
@@ -1,6 +1,6 @@
 #version 330 core
 
-out vec4 outColor;
+out vec4 fragColor;
 
 uniform sampler2D HologramTexture;
 uniform Color col;
@@ -9,12 +9,12 @@ uniform float hologramSpeed;
 uniform float transparencyLevel;
 
 void main() {
-    outColor = rgba(col);
+    fragColor = rgba(col);
     
     vec2 uv = tile(HologramTexture, vec2(1, hologramLinesTiling));
     uv.y -= time * hologramSpeed;
 
     vec4 hologramLines = texture(HologramTexture, uv);
     float alpha = (hologramLines.r + hologramLines.g + hologramLines.b) / 3.0;
-    outColor.a = alpha / transparencyLevel;
+    fragColor.a = alpha / transparencyLevel;
 }
